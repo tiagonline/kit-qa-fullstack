125d3f0f31141e4fe30d367c362a6ef59634465d
Tiago Oliveira Silva <tiago.silva@minu.co>
2026-01-30 15:53:38 -0300

    ajuste yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,43 +2,38 @@ import { defineConfig, devices } from "@playwright/test";
 import * as dotenv from 'dotenv';
 import path from 'path';
 
-// Se ENV_FILE for passado (pelo script), usa ele. 
-// Senão, tenta o .env da raiz.
+// Carrega variáveis de ambiente
 const envPath = process.env.DOTENV_CONFIG_PATH || '.env'; 
 dotenv.config({ path: path.resolve(__dirname, envPath) });
-
-dotenv.config();
 
 export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
-  // Se tiver algum "test.only" no código, falha imediatamente no CI, para evitar que testes sejam esquecidos
-  forbidOnly: !!process.env.CI,//
-  // Retries: Se falhar, quantas vezes tenta de novo se rolar flakiness
+  forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   
   reporter: [
-    ['html', { open: 'never' }],
-    ['list']
+    ['list'],
+    ['allure-playwright', { 
+      detail: true,
+      outputFolder: 'allure-results',
+      suiteTitle: false 
+    }]
   ],
 
   use: {
     headless: true,
-    // Esta baseURL serve como "Padrão" para testes de UI (Web/Mobile).
-    // Assim, nos testes visuais fiz apenas page.goto('/').
-    // Nos testes de API, sobrescrevo isso com a url completa.
     baseURL: process.env.BASE_URL,    
     video: "on",
     trace: "on",
     screenshot: "only-on-failure",
   },
 
-projects: [
+  projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-      // Ignora os testes mobile (assim não roda duplicado nem tenta rodar mobile em desktop)
       testIgnore: '**/mobile/**', 
     },
     {
